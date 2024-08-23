6aca504ac24ece697bc0390f70963b0ff91d4ac4
Coutinh00 <lucasconsentino57@gmail.com>
2024-08-23 20:11:19 -0300

    Importando os componentes de rota no Mains.tsx

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './roules/Home/index.tsx';
+import Produtos from './roules/Produtos/index.tsx';
+import EditarProdutos from './roules/EditarProdutos/index.tsx';
+import Error from './roules/Error/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
