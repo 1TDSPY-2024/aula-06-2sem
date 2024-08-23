276eda3a0e295494b9c19fa79e49f77843d1d136
arthur-pulini <arthurpulini@gmail.com>
2024-08-23 20:05:19 -0300

    Criando os componentes de rotas

---
+++ aula06/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,9 @@
+export default function EditarProdutos() {
+    return (
+      <div>
+        <h1>
+          Componente EditarProdutos
+        </h1>
+      </div>
+    );
+  }
---
+++ aula06/src/routes/Error/index.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+      <div>
+        <h1>
+          Componente Error
+        </h1>
+      </div>
+    );
+  }
