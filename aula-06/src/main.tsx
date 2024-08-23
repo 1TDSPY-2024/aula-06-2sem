c5b8b19f41872c584b30850175db5c4c173898c0
vitorvhsilva <dndragonbr@gmail.com>
2024-08-23 19:38:55 -0300

    Criação das rotas

---
+++ aula-06/src/main.tsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import Error from './routes/Error/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <Home/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
