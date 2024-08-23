73bfd14f1f4a2708c3f4d36209c25fe177ba962d
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-08-23 20:24:58 -0300

    Criando rotas

--- aula06/src/App.tsx
+++ aula06/src/App.tsx
@@ -1,7 +1,9 @@
+import { Route } from "react-router-dom"
 export default function App(){
   return (
     <div>
       <h1>Componente app</h1>
+      <Route path='EditarProdutos/index.tsx'></Route>
     </div>
   )
 }
--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./routes/Home/index.tsx";
+import Produtos  from './routes/Produtos/index.tsx';
+import Error from './routes/Error/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
 
+const routes = createBrowserRouter([
+  {path:"/",element:<App></App>,errorElement:<Error></Error>, children:[
+    {path:"/",element:<Home></Home>},
+    {path:"/produtos",element:<Produtos/>},
+    {path:"/editar/produtos/:id",element:<EditarProdutos/>}
+  ]}
+
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App /> 
+
   </StrictMode>,
 )
