c740a28544debb1e78a80894f9fd2dfa79d707d6
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-08-23 20:56:41 -0300

    css modules aplicando no container do app

--- aula06/src/App.tsx
+++ aula06/src/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet} from "react-router-dom"
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Rodape from "./components/Rodape/Rodape"
+import styles from "./App.module.css";
 export default function App(){
   return (
-    <div>
+    <div className={styles.container}>
       <Cabecalho/>
       <Outlet />
       <Rodape/>
