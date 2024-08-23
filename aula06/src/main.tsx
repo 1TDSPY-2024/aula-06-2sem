806943f07c4b082cb9bb418f5306694dc12f5ab0
Coutinh00 <lucasconsentino57@gmail.com>
2024-08-23 20:34:05 -0300

    Componente Outlet criado e rotas testadas

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Home from './roules/Home/index.tsx';
 import Produtos from './roules/Produtos/index.tsx';
 import EditarProdutos from './roules/EditarProdutos/index.tsx';
 import Error from './roules/Error/index.tsx';
 
-const roules = createBrowserRouter([
+const routes = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/>, children:[
     {path:"/", element:<Home/>},
     {path:"/produtos", element:<Produtos/>},
@@ -17,6 +17,6 @@ const roules = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
