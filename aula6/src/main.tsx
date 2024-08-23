2e59d1773ebc592ca2df0c9c5cc41afd009e3c26
vn740 <vininery206@gmail.com>
2024-08-23 20:34:25 -0300

    Componente Outlet criado e rotas testadas.

--- aula6/src/main.tsx
+++ aula6/src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
+import Home from './routes/Home/index.tsx';
+import Produto from './routes/Produtos/index.tsx';
+
+const routes = createBrowserRouter([
+  {
+    path: "/", element: <App />, errorElement: <Error />, children: [
+      { path: "/", element: <Home /> },
+      { path: "/Produtos", element: <Produto /> },
+      { path: "/editar/produtos/:id", element: <EditarProdutos /> },
+
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
