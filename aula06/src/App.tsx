b09ff2b2a2257eea6a1da41779fc689538ed6e95
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-23 21:01:17 -0300

    Criação de links com componente Link e apresentando CSS.MODULES.

--- aula06/src/App.tsx
+++ aula06/src/App.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
+import styles from "./App.module.css";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.container}>
+      <Cabecalho />
       <Outlet />
+      <Rodape />
     </div>
   );
 }
