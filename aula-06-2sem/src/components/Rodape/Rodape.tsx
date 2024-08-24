84db4c40eecc9e3c32267437c3784ccadf25ef4c
victorcosmedev <victorcosme.profissional@gmail.com>
2024-08-23 21:01:34 -0300

    Final da aula

---
+++ aula-06-2sem/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <footer>
+            <h2>Craido nos Estados Unidos do Brasil...</h2>
+        </footer>
+    );
+}
--- aula-06-2sem/src/routes/Error/index.tsx
+++ aula-06-2sem/src/routes/Error/index.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Error(){
     return(
       <div>
-        Componente Error
+        <h1>Erro 404 - Página não encontrada</h1>
+        <div>
+          <p><Link to="/">Home</Link></p>
+        </div>
       </div>
     );
   }
