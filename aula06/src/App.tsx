e6a86875a1205d8889ebdea27b189df1bbdf1408
MatheusGmota <livematgreg@gmail.com>
2024-08-26 11:58:49 -0300

    Importando componentes para o App

--- aula06/src/App.tsx
+++ aula06/src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Cabecalho from "./components/Cabecalho/Cabecalho"
+import Rodape from "./components/Rodape/Rodape"
+
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Cabecalho/>
+      <Outlet/>
+      <Rodape/>
+    </div>
   )
 }
 
