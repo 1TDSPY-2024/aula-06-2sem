5ab73eefb728a165d4850a6221f1811f45e6cd83
MatheusGmota <livematgreg@gmail.com>
2024-08-26 12:12:12 -0300

    simulando uma lista de produtos

---
+++ aula06/src/listaProduto.ts
@@ -0,0 +1,10 @@
+export const listaprodutos:{id:number, nome: string,preco:number}[] = [
+    {id:1, nome:'Teclado', preco: 239},
+    {id:2, nome:'Mouse', preco: 500},
+    {id:3, nome:'Monitor', preco: 1123},
+    {id:4, nome:'Fone de Ouvido', preco: 259},
+    {id:5, nome:'Cadeira Gamer', preco: 979},
+    {id:6, nome:'Web Cam', preco: 158},
+    {id:7, nome:'Mouse Pad', preco: 119},
+    {id:8, nome:'Filtro de Linha', preco: 120}
+]
