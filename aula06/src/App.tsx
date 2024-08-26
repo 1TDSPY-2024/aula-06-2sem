9898dd091b8becec3f5c7b018d9f76a8d0dc3f98
MatheusGmota <livematgreg@gmail.com>
2024-08-26 11:59:37 -0300

    Importando os estilos do css modules para o App

--- aula06/src/App.tsx
+++ aula06/src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Cabecalho from "./components/Cabecalho/Cabecalho"
+import styles from "./App.module.css"
 import Rodape from "./components/Rodape/Rodape"
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <Cabecalho/>
       <Outlet/>
       <Rodape/>
