0aadcd1c2a8e98913eae0ab04dd2120e7f50bc2a
MatheusGmota <livematgreg@gmail.com>
2024-08-27 19:26:52 -0300

    Listando produtos

--- aula06/src/routes/Produtos/index.tsx
+++ aula06/src/routes/Produtos/index.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
+import styles from "../../App.module.css"
+import { listaprodutos } from "../../listaProduto"
+
 export default function Produtos() {
     return (
-        <div>
+        <div className={styles.produtos}>
             <h1>Produtos</h1>
+            {listaprodutos.map((prod)=>(
+                <div key={prod.id}>
+                    {prod.nome} - <Link to={`/editar-produto/:${prod.id}`}>Editar o Produto</Link>
+                </div>
+            ))}
         </div>
     )
 }
