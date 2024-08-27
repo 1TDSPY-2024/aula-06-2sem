217db06d0c53dbf5ce28a0269c5a528ec1e404f6
MatheusGmota <livematgreg@gmail.com>
2024-08-27 19:28:37 -0300

    Criando componente Menu

--- aula06/src/components/Menu/Menu.tsx
+++ aula06/src/components/Menu/Menu.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
+import styles from "../../App.module.css"
 
 export default function Menu() {
     return(
-        <nav className="menu">
+        <nav className={styles.nav}>
             <Link to='/'>Home</Link>
             <span> | </span>
             <Link to='/produtos'>Produtos</Link>
